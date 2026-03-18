34d1d9e7b5b4088d38a5fd8090a9d0038f7fc4dd
eduardoadf2020-hash <eduardoadf2020@gmail.com>
2026-03-18 19:16:05 -0300

    Acréscimo de um IF

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector('.input-min').value);
     const max = Math.floor(document.querySelector('.input-max').value);
 
+    if (min >= max) {
+        alert('O primeiro número deve ser MENOR que o segundo');
+    }
+
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
     displayResult(result);
